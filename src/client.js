be9e90b861eaeacd055737ec3685e6157a60fff0
vasylyn <v-amons2@i.ua>
2018-11-17 12:59:36 +0200

    Kedr livanskiy bez nozhek i sluha zaebis'

--- src/client.js
+++ src/client.js
@@ -1,1 +1,6 @@
 console.log('gotta secret/??/???');
+console.log('#zapolnit vse');
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', () => {
+
+         cell.textContent = 'X';
+}));
